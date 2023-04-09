88426bebd92ae1c1e0ab76380000a6e20215cf3a
Davidhzk2 <guffyherrera150@gmail.com>
2023-04-08 23:16:09 -0500

    Mejorando la interación con el usuario y tratando de imprimir imagenes

--- chatgpt.py
+++ chatgpt.py
@@ -2,18 +2,42 @@ import openai
 
 openai.api_key = "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"
 
-while(True):
+def createText():
+    while(True):
 
-    prompt = input("\n Ingrese su pregunta: ")
+        prompt = input("\n Ingrese su pregunta: ")
 
-    if prompt == "exit" or prompt == "close":
-        break;
+        if prompt == "exit" or prompt == "close":
+            break;
 
-    completion = openai.Completion.create(engine="text-davinci-003",  
-                            prompt= prompt, 
-                            n = 2,                                         
-                            max_tokens=2048,                                 
-                            );
+        completion = openai.Completion.create(engine="text-davinci-003",  
+                                prompt= prompt, 
+                                n = 2,                                         
+                                max_tokens=2048,                                 
+                                );
 
-    print(completion.choices[0].text)
-        
+        print(completion.choices[0].text)
+
+def createImg():
+
+    prompt = input("\n Ingrese text para crear una imagen: ")
+
+    image = openai.Image.create(prompt= prompt);
+
+    print(image.choices)
+
+
+# ejecucion
+entrada = input("\n Querieres  consultar una imagen o un texto ?: TEXT(1) IMAGE(2)")
+
+if entrada == "1":
+    createText()
+elif entrada == "2":
+    createImg()
+else:
+    print("No Podemos hacer nada, tu ingresaste \n"+ entrada)
+
+    
+
+
+     
