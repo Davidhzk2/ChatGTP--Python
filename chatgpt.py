a9781aa0b63587650de4b071c05ac1ee250699ba
Davidhzk2 <guffyherrera150@gmail.com>
2023-04-08 22:33:30 -0500

    Quitando los comentarios del codigo

--- chatgpt.py
+++ chatgpt.py
@@ -1,10 +1,6 @@
 import openai
 
-# Clave que se asocia a mi usuario
 openai.api_key = "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"
-# modulo openai y la clase Completion - operacion create 
-# parametros habituales
-
 
 while(True):
 
@@ -13,10 +9,10 @@ while(True):
     if prompt == "exit" or prompt == "close":
         break;
 
-    completion = openai.Completion.create(engine="text-davinci-003",        #hace referncia al modelo entrenado 
-                            prompt= prompt, #Pregunta que se le va realizar al gestor
-                            n = 2,                                         #Numero de respuestas que recibimos
-                            max_tokens=2048,                                #Maxima respuesta que nos va a dar admitiendo 4096
+    completion = openai.Completion.create(engine="text-davinci-003",  
+                            prompt= prompt, 
+                            n = 2,                                         
+                            max_tokens=2048,                                 
                             );
 
     print(completion.choices[0].text)
