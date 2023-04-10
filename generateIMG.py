523bfe6d1e2ae82cbda7034c265f31b3b1bfedbf
Davidhzk2 <guffyherrera150@gmail.com>
2023-04-09 20:37:02 -0500

    Cambio del nombre image por el response

--- generateIMG.py
+++ generateIMG.py
@@ -5,13 +5,13 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 try:
     prompt = input("\n Ingrese text para crear una imagen: ")
-    image = openai.Image.create(prompt = prompt,
+    response = openai.Image.create(prompt = prompt,
                                 n=2,
                                 response_format = "url");
     # print(image.data[0 + "\n" + image.data)
     # print(image)
     # print(image.data)
-    print(image.data[0])
+    print(response.data[0])
 
 except NameError :
     print("Something went wrong")
